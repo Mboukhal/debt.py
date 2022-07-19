45bbdd6a9b64cb4bead2f9e7abb3bcb282b4b113
amin <lios80466@gmail.com>
2022-07-19 20:10:35 +0100

    Tue Jul 19 20:10:35 +01 2022

--- debt.py
+++ debt.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+
+	Programe for debt saving 1.0
+
+"""
+
 import os
 import time
 import pandas as pd
@@ -8,7 +14,7 @@ def pause(msg):
 	if os.name == 'posix': # if mac os
 		os.system(f"/bin/bash -c 'read -s -n 1 -p \"{msg}\"'")
 	elif os.name == 'nt': # if windows
-		os.system("pause")
+		os.system(f"echo \"{msg}\" ;pause")
 
 
 def print_list():
@@ -17,15 +23,23 @@ def print_list():
 		print(df.tail(5))
 		print('\n')
 		return
-	print ("No file named \{debt.csv\}")
+	print ("Welcome to debt saver.\n")
 	
 def add_to_csv():
 	print_list()
 	new_row = { 'Name':[], 'Debt':[], 'Date':[] }
+	print('Enter [C] for cancel\n\n')
 	new_row['Name'] = [(input('Enter Name:\n\t>> '))]
-	new_row['Debt'] = [(input('Enter debt:\n\t>> ') + '$')]
+	if len(new_row['Name'][0]) == 1 and new_row['Name'][0][0] == 'C':
+		return
+	new_row['Debt'] = [(input('Enter debt:\n\t>> '))]
+	if (not (new_row['Debt'][0].isnumeric())) and new_row['Debt'][0][0] == 'C':
+		return
+	new_row['Debt'][0] = new_row['Debt'][0] + '$'
 	date_c = time.strftime("%d-%m-%Y %H:%M")
-	new_row['Date'] = [(input(f'Enter date default [{date_c}]:\n\t>> ') or date_c)]
+	new_row['Date'] = [(input(f'Enter date default date [{date_c}]:\n\t>> ') or date_c)]
+	if len(new_row['Date'][0]) == 1 and new_row['Date'][0][0] == 'C':
+		return
 	new_df = pd.DataFrame(new_row)
 	print (new_row)
 	if os.path.exists('debt.csv'):
@@ -45,17 +59,16 @@ def print_list_all():
 	print ("No file named \{debt.csv\}")
 
 def delet_all():
-	print_list()
-	confirm = str(input('Are you sure you want to delet all [Yes/No]: ' or 'No'))
-	if confirm == 'Yes':
-		confirm = str(input('last stape delet all [Yes/No]: ' or 'No'))
-		if confirm == 'Yes':
-			os.remove('debt.csv')
-			pause('debt.csv is deleted!')
-			exit(0)
-	
-		
-		
+	if os.path.exists('debt.csv'):
+		print_list()
+		confirm = str(input('Are you sure you want to delet all [ yes / no ]: ' or 'no'))
+		if confirm == 'yes':
+			confirm = str(input('last stape delet all [ yes / no ]: ' or 'no'))
+			if confirm == 'yes':
+				os.remove('debt.csv')
+				pause('All data deleted.\n')
+				exit(0)
+	pause('No data to delete!\n')
 	
 def delet_from_csv():
 	if os.path.exists('debt.csv'):
@@ -75,7 +88,7 @@ def delet_from_csv():
 		pause('\ndeleted.')
 		df.to_csv('debt.csv', index=False)
 		return
-	print ("No file named \{debt.csv\}")
+	print ("No data to delete!")
 
 def main():
 	cmd = ''
@@ -83,23 +96,20 @@ def main():
 		os.system('clear')
 		print_list()
 		print("\t1. Add debt.")
-		print("\t2. Delet debt.")
-		print("\t3. List debt.")
+		print("\t2. List debt.")
+		print("\t3. Delet debt.")
 		print("\t4. delete All.")
 		print("\t5. To quit.")
 		print()
 		cmd = input('>> ')
+		os.system('clear')
 		if cmd == '1':
-			os.system('clear')
 			add_to_csv()
 		if cmd == '2':
-			os.system('clear')
-			delet_from_csv()
-		if cmd == '3':
-			os.system('clear')
 			print_list_all()
+		if cmd == '3':
+			delet_from_csv()
 		if cmd == '4':
-			os.system('clear')
 			delet_all()
 		if cmd == '5':
 			exit(0)
