6aa5b0fd43a58f1e355e34927e02e7181fb3f144
amin <lios80466@gmail.com>
2022-07-21 15:25:04 +0100

    Thu Jul 21 15:25:04 +01 2022

--- debt.py
+++ debt.py
@@ -12,13 +12,17 @@ import pandas as pd
 
 csv_file = os.path.join(os.getcwd(), 'src/debt.csv')
 log_file = os.path.join(os.getcwd(), 'src/debt.log')
+dir_d = os.path.join(os.getcwd(), 'src')
+if not os.path.exists(dir_d):
+	os.mkdir(dir_d, 0o777)
+	
 
 print (log_file)
 def pause(msg):
 	if os.name == 'posix': # if mac os
 		os.system(f"/bin/bash -c 'read -s -n 1 -p \"{msg}\"'")
 	elif os.name == 'nt': # if windows
-		os.system(f"echo \"{msg}\" ;pause")
+		os.system(f"echo \"{msg}\" &pause>nul")
 
 def log_file_add(msg_log):
 	msg_log = msg_log + ": " + time.strftime("%d-%m-%Y %H:%M:%S") + '\n'
@@ -50,11 +54,9 @@ def add_to_csv():
 			new_row['Debt'][0] = float(new_row['Debt'][0])
 		except ValueError:
 			print ('`', new_row['Debt'][0], '` not expected expresion!')
-			# print (new_row['Debt'][0])
 		else:
 			x = 0
-			
-	new_row['Debt'][0] = str(new_row['Debt'][0]) + '$'
+	new_row['Debt'][0] = "${:,.2f}".format(float(new_row['Debt'][0])) 
 	date_c = time.strftime("%d-%m-%Y %H:%M")
 	new_row['Date'] = [(input(f'Enter date default date [{date_c}]:\n\t>> ') or date_c)]
 	if len(new_row['Date'][0]) == 1 and new_row['Date'][0][0] == 'C':
@@ -62,6 +64,8 @@ def add_to_csv():
 	log_file_add(f"Debt added: [{new_row['Name'][0]}] [{new_row['Debt'][0]}] [{new_row['Date'][0]}] ")
 	new_df = pd.DataFrame(new_row)
 	print (new_row)
+	if not os.path.exists(dir_d):
+		os.mkdir(dir_d, 0o777)
 	if os.path.exists(csv_file):
 		df = pd.read_csv(csv_file)
 		df = df.append(new_df, ignore_index=True)
@@ -84,11 +88,18 @@ def delet_all():
 		print_list()
 		confirm = str(input('Are you sure you want to delet all [ yes / no ]: ' or 'no'))
 		if confirm == 'yes':
-			confirm = str(input('last stape delet all [ yes / no ]: ' or 'no'))
-			if confirm == 'yes':
+			csv = str(input(f'Press [y] to remove {csv_file}:\n\t>> ' or ''))
+			if csv == 'y':
 				os.remove(csv_file)
+			log = str(input(f'Press [y] to remove {log_file}:\n\t>> ' or ''))
+			if log == 'y':
+				os.remove(log_file)
+			if log == 'y' and csv == 'y':
+				path = os.path.join(os.getcwd(), 'src')
+				os.rmdir(path)
 				pause('All data deleted.\n')
-	pause('No data to delete!\n')
+			return
+	pause('Deleting canceled!\n')
 	
 def delet_from_csv():
 	if os.path.exists(csv_file):
